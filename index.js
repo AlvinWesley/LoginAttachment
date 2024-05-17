3d7573e95840f5f6e5dd90dd3f4380fec237a066
alvin <wesealv@gmail.com>
2024-05-17 12:55:46 +0300

    newWebPage

---
+++ index.js
@@ -0,0 +1,7 @@
+// var btn=document.querySelector(".btn");
+// btn.addEventListener('click',function(){ alert('Hello alvin')});
+var forgot=document.querySelector(".btn");
+forgot.addEventListener('click',showing());
+function showing(){
+    alert("showing the function");
+}
